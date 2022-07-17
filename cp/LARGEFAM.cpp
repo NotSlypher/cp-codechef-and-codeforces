adfbc6d7e127c48553c3a192b080be0f30a1ca50
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-07-17 23:34:46 +0530

    CodeChef LunchTime july 2022

---
+++ cp/LARGEFAM.cpp
@@ -0,0 +1,74 @@
+﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; if(x == 0) p++; else v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+void Slypher()
+{
+	ll n, p = 0, cnto = 0, q = 1;
+	vll a;
+	cin >> n;
+	vin(a, n);
+	sort(all(a));
+	for (ll i = 0; i < p; i++) a.push_back(0);
+
+	for (ll i = 0; i < n; i++) {
+		int f = 1;
+		for (ll k = 0; k < a[i]; k++) {
+			if (n <= q)
+			{
+				f = 0;
+				break;
+			}
+			else q++;
+		}
+		cnto += f;
+	}
+	cout << cnto << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
---
+++ cp/MEDMAXMATCH.cpp
@@ -0,0 +1,67 @@
+﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+void Slypher()
+{
+	ll n, maxa = 0;
+	cin >> n;
+	vll a, b, f(n);
+	vin(a, n)
+	vin(b, n);
+	sort(all(a));
+	sort(all(b), greater<int>());
+	for (int i = 0; i < n/2; ++i)
+		f[i] = a[i] + b[i + n / 2];
+	for (int i = 0; i < n/2 + 1; ++i)
+		f[i + n/2] = a[i + n/2] + b[i];
+	sort(all(f));
+	cout << f[n / 2] << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
