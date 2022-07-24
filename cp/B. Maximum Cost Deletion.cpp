bb4eadfe843cb70768dcb8f68b7f60367835d93d
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-07-25 01:18:52 +0530

    codeforces

---
+++ cp/B. Maximum Cost Deletion.cpp
@@ -0,0 +1,60 @@
+﻿﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+void Slypher()
+{
+	ll n, a, b, sum = 0, j = 0;
+	string s;
+	cin >> n >> a >> b >> s;
+	vll parts(n);
+	int m = unique(s.begin(), s.end()) - s.begin();
+	cout << a * n + (b * n, b * (m / 2 + 1)) << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
