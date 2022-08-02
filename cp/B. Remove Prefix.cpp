388a1f40eb3a4d7eea51d441b8f8eafb8abd275b
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-08-02 17:48:12 +0530

    Codeforces

---
+++ cp/B. Remove Prefix.cpp
@@ -0,0 +1,70 @@
+﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+void Slypher()
+{
+	ll n, i;
+	vll a;
+	cin >> n;
+	vin(a, n);
+	map<ll, ll> b;
+	reverse(all(a));
+	for ( i = 0; i < n; ++i)
+	{
+		if(b[a[i]] == 1)
+		{
+			cout << n - i << endl;
+			return;
+ 		}
+		b[a[i]]++;
+	}
+	cout << 0 << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
