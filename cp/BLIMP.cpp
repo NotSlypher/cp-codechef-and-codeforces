b2d7150dbcfedf63f89493fec042fcbe354b774d
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-08-04 00:37:01 +0530

    codechef starters 50

---
+++ cp/BLIMP.cpp
@@ -0,0 +1,83 @@
+﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+#define dopos a[i] = max(0ll, a[i]);
+
+void Slypher()
+{
+	ll n, x, y, mxnum, ceilnum, sumsubs = 0, ans = 0;
+	cin >> n >> x >> y;
+	vll a;
+	vin(a, n);
+	mxnum = *max_element(all(a));
+	if (x<=y)
+	{
+		if (mxnum <= 0)
+			cout << "0\n";
+		else
+			cout << (mxnum + y - 1) / y << endl;
+	}
+	else
+	{
+		for (int i = 0; i < n; ++i)
+			dopos;
+		for (ll i = n - 1ll; i >= 0; --i)
+		{
+			a[i] -= sumsubs;
+			dopos;
+			ceilnum = (a[i] + x - 1) / x;	
+			sumsubs += y * ceilnum;
+			ans += ceilnum;
+		}
+		cout << ans << endl;
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
---
+++ cp/SUMPRODSEG.cpp
@@ -0,0 +1,79 @@
+﻿/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+#define check if (low.first <= low.second){ flag = true; break;}
+
+void Slypher()
+{
+	ll x, y;
+	vll a;
+	pair<ll, ll> low, high;
+	bool flag = 0;
+	cin >> x >> y;
+	for (ll i = 1; i <= sqrt(y); ++i)
+		if (y % i == 0)
+			y / i == i ? a.push_back(i) : a.push_back(i), a.push_back(y / i);
+	sort(all(a));
+	for (long long value : a)
+	{
+		if (value <= y / value)
+		{
+			high = { value, y / value };
+			low = { x - (value - 1), value - 1 };
+			check
+			low = { y / value + 1, x - (y / value + 1) };
+			check
+		}
+		else
+			break;
+	}
+	flag ? cout << low.first << " " << low.second << endl << high.first << " " << high.second << endl :cout << -1 << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
