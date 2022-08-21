3de79770b419cdcf82886a3ace293c98ff93a706
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-08-21 09:24:49 +0530

    Codeforces Round #816 (Div. 2)

---
+++ cp/A. Crossmarket.cpp
@@ -0,0 +1,62 @@
+/*
+Slypherx - Ayush Gupta
+Pune Institute of Computer Technology
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector<ll>
+#define vin(v,n)     for(ll i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define all(xx)       xx.begin(), xx.end()
+
+const int MOD = 1e9 + 7;
+int fpow(int x, int y);
+int gcd(int a, int b);
+
+void Slypher()
+{
+	ll h, w;
+	cin >> h >> w;
+	if(h == w && w == 1)
+	{
+		cout << "0\n";
+		return;	
+	}
+	cout << h + w - 2 + min(h, w) << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int test = 1; test <= t; ++test)
+	{
+		Slypher();
+	}
+	return 0;
+}
+
+int fpow(int x, int y)
+{
+	x = x % MOD;
+	int sum = 1;
+	while (y)
+	{
+		if (y & 1)sum = sum * x;
+		sum %= MOD;
+		y = y >> 1;
+		x = x * x;
+		x %= MOD;
+	}
+	return sum;
+}
+
+int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
